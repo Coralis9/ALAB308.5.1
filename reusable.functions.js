3be008139c71226cb3999fc14157f09bdf13354d
Coralis <coralis.jpg@gmail.com>
2025-03-07 11:45:56 -0500

    commit

--- reusable.functions.js
+++ reusable.functions.js
@@ -7,15 +7,15 @@ function sumArray(numbers) {
     return numbers.reduce((sum, num) => sum + num, 0);
 }
 
-function averageArray(numbers) {
+function Array(numbers) {
     return numbers.length === 0 ? 0 : sumArray(numbers) / numbers.length;
 }
 
-function longestString(strings) {
+function str.length(strings) {
     return strings.reduce((longest, str) => str.length > longest.length ? str : longest, "");
 }
 
-function stringsLongerThan(strings, length) {
+function str.length(strings, length) {
     return strings.filter(str => str.length > length);
 }
 
@@ -31,22 +31,15 @@ function printNumbers(n, current = 1) {
 
 // creating a chart 
 
-function sortByAge(people) {
+function (people) {
     return people.sort((a, b) => a.age - b.age);
 }
 
-function filterByAge(people) {
-    return people.filter(person => person.age <= 50);
+function (people) {
+    return people.sort(person => person.age <= 50);
 }
 
-function transformPeople(people) {
-    return people.map(person => ({
-        ...person,
-        age: person.age + 1,
-        job: person.occupation,
-    }));
-}
-function sumAges(people) {
+function (people) {
     return people.reduce((sum, person) => sum + person.age, 0);
 }
 
